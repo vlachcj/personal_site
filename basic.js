7dcc6b6efe3f85b7f93893c6de324743d192db34
vlachcj <vlachcj@umich.edu>
2015-09-16 11:57:17 -0600

    tab anchors, font awesome icons, updated info, adjusted home clickable area

--- basic.js
+++ basic.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-	$("#name").on("click", function() {
-  		$("#menu_tabs a div").each(function() {
-    		if($(this).hasClass("active")) {
-       			$(this).removeClass("active");
-    		} 
-  		});
-	});
+  $("#name").children().on("click", function() {
+      $("#menu_tabs a div").each(function() {
+        if($(this).hasClass("active")) {
+            $(this).removeClass("active");
+        } 
+      });
+      window.location.hash = "bio";
+  });
 	$("._col:not(#resume)").on("click", function() {
 		 $("._col").each(function() {
     		if($(this).hasClass("active")) {
@@ -14,4 +15,13 @@ $(document).ready(function() {
   		});
 		$(this).addClass("active");
 	});
+  if (location.hash !== '') {
+    $('#menu_tabs a[href="' + location.hash + '-tab"]').tab('show');
+    $('#menu_tabs a[href="' + location.hash + '-tab"] div').addClass("active");
+  } else {
+    $('#menu_tabs a[href="bio-tab"]').tab('show');
+  }
+  $('#menu_tabs a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+      window.location.hash = e.target.hash.substr(1).replace("-tab","") ; 
+  });
 });
